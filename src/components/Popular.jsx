9326655d97a56ad9091bef3f4acf0a94abdb3052
SureshKuchana <kuchana123.suresh@gmail.com>
2021-06-12 20:06:10 +0530

    added util folder for api to get the repos from github

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,12 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
 
-const Popular = () => {
-  const [lang, setLang] = React.useState("All");
+const LanguagesNav = ({ selectedLang, onUpdateLanguage }) => {
   const langauges = ["All", "JavaScript", "Python", "Ruby", "CSS", "Rust"];
-
-  const updateLanguage = (selectedLang) => {
-    setLang(selectedLang);
-  };
 
   return (
     <ul className="flex-center">
@@ -15,14 +12,52 @@ const Popular = () => {
           <li key={index}>
             <button
               className="btn-clear nav-link"
-              style={langauge === lang ? { color: "rgb(187,46,31)" } : null}
-              onClick={() => updateLanguage(langauge)}
+              style={
+                langauge === selectedLang ? { color: "rgb(187,46,31)" } : null
+              }
+              onClick={() => onUpdateLanguage(langauge)}
             >
               {langauge}
             </button>
           </li>
         ))}
     </ul>
+  );
+};
+
+LanguagesNav.propTypes = {
+  selectedLang: PropTypes.string.isRequired,
+  updateLanguage: PropTypes.func.isRequired,
+};
+
+const Popular = () => {
+  const [lang, setLang] = React.useState("All");
+  const [repos, setRepos] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  const updateLanguage = (selectedLang) => {
+    setLang(selectedLang);
+    setError(null);
+    setRepos(null);
+
+    fetchPopularRepos(selectedLang)
+      .then((repos) => setRepos(repos), setError(null))
+      .catch((error) => {
+        console.warn("Error fetching repos : ", error);
+        setError(" There was an error fetching the repositories");
+      });
+  };
+
+  const isLoading = () => {
+    return repos === null && error === null;
+  };
+  return (
+    <>
+      <LanguagesNav onUpdateLanguage={updateLanguage} selectedLang={lang} />
+      {isLoading && <p>LOADING</p>}
+      {error && <p>{error}</p>}
+      {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+    </>
   );
 };
 
